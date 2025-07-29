19bc866ca885d6b05fb39182f66d0eb6e4f8d7de
Dihubopen <dihubcn@gmail.com>
2025-07-29 19:47:39 +0800

    fix CORS

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,6 +10,8 @@ func CORS() gin.HandlerFunc {
 	config.AllowAllOrigins = true
 	config.AllowCredentials = true
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"*"}
+	// config.AllowHeaders = []string{"*"}
+	config.AllowHeaders = []string{"*", "Authorization", "New-Api-User"}
+	config.MaxAge = 43200 // 12小时
 	return cors.New(config)
 }
